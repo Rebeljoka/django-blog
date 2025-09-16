470b9e3a7bb4f8b9a451f133ac8de9ed609f25e9
Rebeljoka <habib.u99@googlemail.com>
2025-09-16 21:19:14 +0100

    Add: Database Settings + SU

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_NPV68QxlHoXE@ep-silent-grass-agkrhw0x.c-2.eu-central-1.aws.neon.tech/roman_elope_truck_247871")
