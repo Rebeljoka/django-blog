61c89dd45334733038ec7f074d0b632a095428fd
Rebeljoka <habib.u99@googlemail.com>
2025-09-16 21:54:36 +0100

    Add: Its a SECRET KEY!

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_NPV68QxlHoXE@ep-silent-grass-agkrhw0x.c-2.eu-central-1.aws.neon.tech/roman_elope_truck_247871")
+os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_NPV68QxlHoXE@ep-silent-grass-agkrhw0x.c-2.eu-central-1.aws.neon.tech/roman_elope_truck_247871")
+os.environ.setdefault("SECRET_KEY", "django-insecure-65m0yzsb*y^-7!3(f@9ekp-!!1*-v%3c+ijcweox8d$gyvhgrf")
